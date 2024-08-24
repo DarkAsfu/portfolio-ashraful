363dfe7351f18549693338d1acdce2cdd8573603
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-08-24 14:55:58 +0600

    Remove api for project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,30 +5,33 @@ import placeholderData from "../../Hooks/data"; // Your placeholder data
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null); // State for error handling
-  console.log(placeholderData);
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const response = await fetch('https://portfolio-backend-0y27.onrender.com/projects');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        console.log(data);
-        setProjects(data.length > 0 ? data : placeholderData); // Use API data if available, otherwise fallback to local data
-      } catch (error) {
-        console.error("Error fetching projects:", error);
-        setProjects(placeholderData); // Fallback to local data on error
-        setError("");
-      } finally {
-        setLoading(false);
-      }
-    };
+  // const [loading, setLoading] = useState(true);
+  useEffect( () => {
+    setProjects(placeholderData)
+  }, [])
+  // const [error, setError] = useState(null); // State for error handling
+  // console.log(placeholderData);
+  // useEffect(() => {
+  //   const fetchProjects = async () => {
+  //     try {
+  //       const response = await fetch('https://portfolio-backend-0y27.onrender.com/projects');
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok');
+  //       }
+  //       const data = await response.json();
+  //       console.log(data);
+  //       setProjects(data.length > 0 ? data : placeholderData); // Use API data if available, otherwise fallback to local data
+  //     } catch (error) {
+  //       console.error("Error fetching projects:", error);
+  //       setProjects(placeholderData); // Fallback to local data on error
+  //       setError("");
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    fetchProjects();
-  }, []);
+  //   fetchProjects();
+  // }, []);
 
   const filterProjects = (category) => {
     if (category === 'All') return projects;
@@ -39,7 +42,7 @@ const Projects = () => {
   return (
     <div className="flex w-full flex-col max-w-screen-xl mx-auto py-10" data-aos="fade-up">
       <h1 className="text-4xl font-extrabold text-center mb-6">My Projects</h1>
-      {error && <div>{error}</div>} {/* Display error message if API fails */}
+      {/* {error && <div>{error}</div>} Display error message if API fails */}
       <Tabs aria-label="Project Categories" className="mb-4 px-4 md:mx-auto">
         <Tab key="All" title="All">
           <ProjectCard projects={filterProjects("All")} />
