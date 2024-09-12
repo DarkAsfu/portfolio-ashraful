69f6dda353ac18a22800083f4ac3d04b93b1ebde
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-09-13 02:54:43 +0600

    Added a animation skills in banner section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,24 +1,51 @@
 import { Link } from "react-router-dom";
 import resume from '../../../public/resume_ashraful.pdf'
+import IconCloud from "../IconCloudDemo/IconCloudDemo";
 const Banner = () => {
+    const slugs = [
+        "typescript",
+        "javascript",
+        "react",
+        "html5",
+        "css3",
+        "nodedotjs",
+        "express",
+        "nextdotjs",
+        "firebase",
+        "vercel",
+        "redux",
+        "cplusplus",
+        "c",
+        "netlify",
+        "mui",
+        "dom",
+        "git",
+        "github",
+        "gitlab",
+        "visualstudiocode",
+        "figma",
+        "mongodb",
+        "tailwindcss",
+        "bootstrap"
+    ];
     return (
         <>
             {/* <div className="fixed top-0 -z-20 h-screen w-full bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div> */}
-            <div>
-                <section className="">
-                    <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-                        <div className="mx-auto max-w-xl text-center">
+            <div className="mx-auto flex max-w-screen-xl justify-between items-center">
+                <section className="text-left">
+                    <div className=" px-4 py-32 lg:flex lg:h-screen lg:items-center">
+                        <div className="mx-auto max-w-xl text-center md:text-left">
                             <h1 className="text-3xl font-extrabold sm:text-[46px] leading-tight">
                                 Are you looking for a
                                 <strong className="font-extrabold text-[#0D9488] sm:block"> Mern Stack Developer? </strong>
                             </h1>
 
                             <p className="mt-4 text-[14px]/relaxed md:text-xl/relaxed">
-                            Creating stunning, functional websites with React. Explore my projects and see my passion for seamless user experiences.
+                                Creating stunning, functional websites with React. Explore my projects and see my passion for seamless user experiences.
                             </p>
 
-                            <div className="mt-8 flex flex-wrap justify-center gap-4">
-                                <a  download
+                            <div className="mt-8 flex flex-wrap gap-4">
+                                <a download
                                     className="block w-full rounded bg-[#0D9488] px-12 py-3 text-sm font-medium text-white shadow hover:bg-[#0D9488] focus:outline-none focus:ring active:bg-[#33a89e] sm:w-auto"
                                     href={resume}
                                 >
@@ -35,6 +62,9 @@ const Banner = () => {
                         </div>
                     </div>
                 </section>
+                <div className="hidden md:block">
+                    <IconCloud iconSlugs={slugs} />
+                </div>
             </div>
         </>
     );
---
+++ src/Components/NoiseTest/NoiseTest.jsx
@@ -0,0 +1,23 @@
+const NoiseTest = () => {
+    return (
+        <div>
+            {/* <main className="relative ">
+                <div
+                    className="absolute top-0 left-0 w-full h-full content-[''] z-10 pointer-events-none bg-[url('https://res.cloudinary.com/dzl9yxixg/image/upload/noise_yvdidf.gif')]"
+                    style={{ opacity: 0.1 }}
+                ></div>
+                <section className="  font-semibold 2xl::h-[600px] sm:h-[500px] h-[450px] bg-gradient-to-t dark:to-gray-800 dark:from-gray-950 to-[#041253] from-[#ebebeb] flex flex-col items-center justify-center dark:text-white text-black">
+                    <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:35px_34px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+
+                    <h1 className="2xl:text-7xl text-5xl px-8 font-semibold text-center tracking-tight leading-[120%]">
+                        An Home Page with Noise Effect
+                    </h1>
+                </section>
+
+            </main> */}
+           
+        </div>
+    );
+};
+
+export default NoiseTest;
